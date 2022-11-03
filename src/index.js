07a2deea5daacf3c3ad6326ec9ca8c95174a555f
alekseikorotkov <korotkovaleksei@icloud.com>
2022-11-03 15:14:01 +0200

    Add css variables

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import './variables.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
